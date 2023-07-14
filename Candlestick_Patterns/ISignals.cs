9db596a067b82644e3a10ac8e0d2603c5afb7726
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-07-14 20:01:17 +0200

    Added dataOhlcv to all ISignals functions;

--- Candlestick_Patterns/ISignals.cs
+++ Candlestick_Patterns/ISignals.cs
@@ -2,12 +2,12 @@
 {
     public interface ISignals
     {
-        int GetBullishSignalsCount();
-        int GetBearishSignalsCount();
-        int GetSignalsCount(string[] patternNames);
-        int GetSignalsCount(string patternName);
-        decimal GetSignalsIndex(string patternName, decimal weight);
-        decimal GetSignalsIndex(Dictionary<string, decimal> patternNamesWithWeights);
-        List<OhlcvObject> GetOhlcvWithSignals(string patternName);
+        int GetBullishSignalsCount(List<OhlcvObject> dataOhlcv);
+        int GetBearishSignalsCount(List<OhlcvObject> dataOhlcv);
+        int GetSignalsCount(List<OhlcvObject> dataOhlcv, string[] patternNames);
+        int GetSignalsCount(List<OhlcvObject> dataOhlcv, string patternName);
+        decimal GetSignalsIndex(List<OhlcvObject> dataOhlcv, string patternName, decimal weight);
+        decimal GetSignalsIndex(List<OhlcvObject> dataOhlcv, Dictionary<string, decimal> patternNamesWithWeights);
+        List<OhlcvObject> GetOhlcvWithSignals(List<OhlcvObject> dataOhlcv, string patternName);
     }
 }
--- Candlestick_Patterns/Signals.cs
+++ Candlestick_Patterns/Signals.cs
@@ -2,5 +2,39 @@
 {
     public class Signals : ISignals
     {
+        public int GetBearishSignalsCount(List<OhlcvObject> dataOhlcv)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBullishSignalsCount(List<OhlcvObject> dataOhlcv)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OhlcvObject> GetOhlcvWithSignals(List<OhlcvObject> dataOhlcv, string patternName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetSignalsCount(List<OhlcvObject> dataOhlcv, string[] patternNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetSignalsCount(List<OhlcvObject> dataOhlcv, string patternName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetSignalsIndex(List<OhlcvObject> dataOhlcv, string patternName, decimal weight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetSignalsIndex(List<OhlcvObject> dataOhlcv, Dictionary<string, decimal> patternNamesWithWeights)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
