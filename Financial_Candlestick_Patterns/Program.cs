93700d46e1fb55acc95385141b0a0f8eeb3fb363
przemyslawbak <przemyslaw.bak@simple-mail.net>
2023-07-14 17:38:42 +0200

    Loading data from GIST file;

--- Financial_Candlestick_Patterns/Program.cs
+++ Financial_Candlestick_Patterns/Program.cs
@@ -2,12 +2,17 @@
 using Newtonsoft.Json;
 
 Patterns _patterns;
-string dataSampleFile = "json_data_sample.txt";
 string json = string.Empty;
 
-using (StreamReader r = new StreamReader(dataSampleFile))
+var client = new HttpClient();
+var url = "https://gist.githubusercontent.com/przemyslawbak/2058d9aeddfe09d2a26da81dfc16e5d0/raw/json_data_sample.txt";
+
+using (HttpResponseMessage response = await client.GetAsync(url))
 {
-    json = r.ReadToEnd();
+    using (HttpContent content = response.Content)
+    {
+        json = content.ReadAsStringAsync().Result;
+    }
 }
 
 var dataOhlcv = JsonConvert.DeserializeObject<List<OhlcvObject>>(json).Select(x => new OhlcvObject()
@@ -22,7 +27,5 @@ var dataOhlcv = JsonConvert.DeserializeObject<List<OhlcvObject>>(json).Select(x 
 _patterns = new Patterns(dataOhlcv);
 var bullishSignalsCount = _patterns.GetBullishSignalsCount();
 var bearishSignalsCount = _patterns.GetBearishSignalsCount();
-
-//todo: add volume weightening
 
 Console.ReadLine();
