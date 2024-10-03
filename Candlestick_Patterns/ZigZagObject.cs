3ca819326f3f53d9cf508eacfac50b4b6daf8d83
Aliuser11 <dajmispokojAZ@gmail.com>
2024-10-03 15:27:25 +0200

    ZigZagObject;

---
+++ Candlestick_Patterns/ZigZagObject.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+
+namespace Candlestick_Patterns
+{
+    public class ZigZagObject
+    {
+        [JsonProperty(PropertyName = "date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty(PropertyName = "close")]
+        public decimal Close { get; set; }
+
+        public bool Signal { get; set; } = false;
+    }
+}
