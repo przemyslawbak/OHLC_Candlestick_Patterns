b0f124f26ae1cd55dcf058778ccbe03c455468e8
Aliuser11 <dajmispokojAZ@gmail.com>
2024-10-04 20:03:04 +0200

    IFormations

---
+++ Candlestick_Patterns/IFormations.cs
@@ -0,0 +1,9 @@
+﻿namespace Candlestick_Patterns
+{
+    internal interface IFormations
+    {
+        List<string> GetFormationsAllMethodNames();
+        List<ZigZagObject> GetFormationsSignals(string patternName);
+        int GetFormationsSignalsCount(string patternName);
+    }
+}
