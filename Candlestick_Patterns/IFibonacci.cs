85ab8b55e949121c4c93a964b131593a90b6fcda
Aliuser11 <dajmispokojAZ@gmail.com>
2024-11-16 18:50:44 +0100

    IFibonacci;

---
+++ Candlestick_Patterns/IFibonacci.cs
@@ -0,0 +1,9 @@
+﻿namespace Candlestick_Patterns
+{
+   internal interface IFibonacci
+   {
+       List<string> GetFibonacciAllMethodNames();
+       List<ZigZagObject> GetFibonacciSignalsQuantities(string patternName);
+       int GetFibonacciSignalsCount(string patternName);
+   }
+}
