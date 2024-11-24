2664c5a1c502c968644508723d07301687f62c27
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-11-24 16:46:54 +0100

    IAccuracyTrials interface added with related class;

---
+++ Candlestick_Patterns/AccuracyTrials.cs
@@ -0,0 +1,12 @@
+﻿using Candlestick_Patterns;
+
+namespace OHLC_Candlestick_Patterns
+{
+    public class AccuracyTrials : IAccuracyTrials
+    {
+        public List<OhlcvObject> GetPatternAccuracy(List<OhlcvObject> dataOhlcv, string patternName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Candlestick_Patterns/IAccuracyTrials.cs
@@ -0,0 +1,16 @@
+﻿using Candlestick_Patterns;
+
+namespace OHLC_Candlestick_Patterns
+{
+    internal interface IAccuracyTrials
+    {
+
+        /// <summary>
+        /// Tests selected pattern accuracy in given data
+        /// </summary>
+        /// <param name="dataOhlcv">OHLC object list</param>
+        /// <param name="patternName">Selected pattern</param>
+        /// <returns>OHLC list with updated signal values</returns>
+        List<OhlcvObject> GetPatternAccuracy(List<OhlcvObject> dataOhlcv, string patternName);
+    }
+}
