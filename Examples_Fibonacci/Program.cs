2bc2aedd830d934c96bc83272b71de81d20e50a2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-11-28 20:35:00 +0100

    Added examples in the projects; Removed old examples project;

--- Examples_Fibonacci/Program.cs
+++ Examples_Fibonacci/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Examples_Fibonacci
+﻿using Candlestick_Patterns;
+using Newtonsoft.Json;
+using OHLC_Candlestick_Patterns;
+
+namespace Examples_Fibonacci
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string json = string.Empty;
+            ISignals _signals = new Signals();
+            IAccuracyTrials _accuracy = new AccuracyTrials();
+            var client = new HttpClient();
+            var url = "https://gist.githubusercontent.com/przemyslawbak/c90528453d512a8d85ad2deea5cf6ad2/raw/aapl_us_d.csv";
+
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                using (HttpContent content = response.Content)
+                {
+                    json = content.ReadAsStringAsync().Result;
+                }
+            }
+
+            var dataOhlcv = JsonConvert.DeserializeObject<List<OhlcvObject>>(json).Select(x => new OhlcvObject()
+            {
+                Open = x.Open,
+                High = x.High,
+                Low = x.Low,
+                Close = x.Close,
+                Volume = x.Volume,
+            }).Reverse().ToList();
+
+            //ACCURACY TRIALS
+            var accuracyForSelectedFiboToTheEndOfDataSet = _accuracy.GetAverPercentFiboAccuracy(dataOhlcv, "Bearish 3 Drive");
+            var accuracyForSelectedFibo30CandlesAhead = _accuracy.GetAverPercentFiboAccuracy(dataOhlcv, "Bearish 3 Drive", 30);
+
+            //SIGNALS
+            var fiboSingle = _signals.GetFibonacciSignalsCount(dataOhlcv, "Bearish 3 Drive");
+            var otherFiboSingle = _signals.GetFibonacciSignalsCount(dataOhlcv, "Bullish 3 Drive");
+
+            //END
+            Console.ReadLine();
         }
     }
 }
--- Examples_Patterns/Program.cs
+++ Examples_Patterns/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace Examples_Patterns
+﻿using Candlestick_Patterns;
+using Newtonsoft.Json;
+using OHLC_Candlestick_Patterns;
+
+namespace Examples_Patterns
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string json = string.Empty;
+            ISignals _signals = new Signals();
+            IAccuracyTrials _accuracy = new AccuracyTrials();
+            var client = new HttpClient();
+            var url = "https://gist.githubusercontent.com/przemyslawbak/c90528453d512a8d85ad2deea5cf6ad2/raw/aapl_us_d.csv";
+
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                using (HttpContent content = response.Content)
+                {
+                    json = content.ReadAsStringAsync().Result;
+                }
+            }
+
+            var dataOhlcv = JsonConvert.DeserializeObject<List<OhlcvObject>>(json).Select(x => new OhlcvObject()
+            {
+                Open = x.Open,
+                High = x.High,
+                Low = x.Low,
+                Close = x.Close,
+                Volume = x.Volume,
+            }).Reverse().ToList();
+
+            //ACCURACY TRIALS
+            var accuracyForSelectedPatternToTheEndOfDataSet = _accuracy.GetAverPercentPatternAccuracy(dataOhlcv, "Bullish 3 Inside Up");
+            var accuracyForSelectedPattern30CandlesAhead = _accuracy.GetAverPercentPatternAccuracy(dataOhlcv, "Bullish 3 Inside Up", 30);
+
+            //SIGNALS
+            var bullishCount = _signals.GetPatternsBullishSignalsCount(dataOhlcv);
+            Console.WriteLine("Bullish signals count: {0}", bullishCount); //Bullish signals count:
+
+            var bearishCount = _signals.GetPatternsBearishSignalsCount(dataOhlcv);
+            Console.WriteLine("Bearish signals count: {0}", bearishCount); //Bearish signals count:
+
+            var signalsCountMulti = _signals.GetPatternsSignalsCount(dataOhlcv, new string[] { "Bearish Belt Hold", "Bearish Black Closing Marubozu" });
+            Console.WriteLine("Multiple patterns signals count: {0}", signalsCountMulti); //Multiple patterns signals count:
+
+            var signalsCountSingle = _signals.GetPatternsSignalsCount(dataOhlcv, "Bearish Black Closing Marubozu");
+            Console.WriteLine("Single pattern signals count: {0}", signalsCountSingle); //Single pattern signals count:
+
+            var signalsCountMultiWeightened = _signals.GetPatternsSignalsIndex(dataOhlcv, new Dictionary<string, decimal>() { { "Bearish Belt Hold", 0.5M }, { "Bearish Black Closing Marubozu", 0.5M } });
+            Console.WriteLine("Weightened index for selected multiple patterns: {0}", signalsCountMultiWeightened); //Weightened index for selected multiple patterns:
+
+            var signalsCountSingleWeightened = _signals.GetPatternsSignalsIndex(dataOhlcv, "Bearish Black Closing Marubozu", 0.5M);
+            Console.WriteLine("Weightened index for selected single pattern: {0}", signalsCountSingleWeightened); //Weightened index for selected single pattern:
+
+            var ohlcSingleSignals = _signals.GetPatternsOhlcvWithSignals(dataOhlcv, "Bearish Black Closing Marubozu");
+            Console.WriteLine("Signals for single pattern: {0}", ohlcSingleSignals.Where(x => x.Signal == true).Count()); //Signals for single pattern:
+
+            var ohlcMultiSignals = _signals.GetPatternsOhlcvWithSignals(dataOhlcv, new string[] { "Bearish Belt Hold", "Bearish Black Closing Marubozu" });
+            Console.WriteLine("Number of lists returned: {0}", ohlcMultiSignals.Count()); //Number of lists returned:
+
+            //END
+            Console.ReadLine();
         }
     }
 }
