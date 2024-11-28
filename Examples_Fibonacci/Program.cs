e237bd1c96d7b1a5e0c803ba0a196acf3b443a5e
przemyslawbak <przemyslaw.bak@simple-mail.net>
2024-11-28 17:16:00 +0100

    Examples blank projects added;

---
+++ Examples_Fibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examples_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Examples_Formations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examples_Formations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
