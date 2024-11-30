8210226ba6fa511c7af14fec0711d7a4da6aef5b
Aliuser11 <dajmispokojAZ@gmail.com>
2024-11-30 08:52:03 +0100

    Simplify naming;

--- GraphCreator/Program.cs
+++ GraphCreator/Program.cs
@@ -2,14 +2,13 @@ using Candlestick_Patterns;
 using System.Windows.Forms.DataVisualization.Charting;
 using GraphMaker;
 
-namespace Dino.Tools.WebMonitor
+namespace GraphCreator
 {
     public class Graph : Form
     {
-        private System.ComponentModel.IContainer components = null;
-        System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-
-        IFiboTester _fiboTester = new FiboTester();
+        private readonly IFiboTester _fiboTester = new FiboTester();
+        private readonly System.ComponentModel.IContainer components = null;
+        Chart chart1;
 
         [STAThread]
         static void Main()
@@ -18,33 +17,34 @@ namespace Dino.Tools.WebMonitor
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Graph());
         }
+
         public Graph()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void LoadData(object sender, EventArgs e)
         {
             chart1.Series.Clear();
-            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            var series1 = new Series
             {
                 Name = "graph",
-                Color = System.Drawing.Color.Green,
+                Color = Color.Green,
                 IsVisibleInLegend = false,
                 IsXValueIndexed = true,
                 ChartType = SeriesChartType.Line
             };
 
-            this.chart1.Series.Add(series1);
+            chart1.Series.Add(series1);
 
             var patternName = "BullishButterfly";
             //var points = GetGraphData(patternName);
             var points = new List<ZigZagObject>()
             {
-                new ZigZagObject() {Close = 2.5M, Signal = false},
-                new ZigZagObject() {Close = 3.5M, Signal = true},
-                new ZigZagObject() {Close = 4.5M, Signal = true},
-                new ZigZagObject() {Close = 4.5M, Signal = false} ,
+                new() {Close = 2.5M, Signal = false},
+                new() {Close = 3.5M, Signal = true},
+                new() {Close = 4.5M, Signal = true},
+                new() {Close = 4.5M, Signal = false} ,
             };
 
             for (int i = 0; i < points.Count; i++)
@@ -78,8 +78,8 @@ namespace Dino.Tools.WebMonitor
 
         private void InitializeComponent()
         {
-            ChartArea chartArea1 = new ChartArea();
-            Legend legend1 = new Legend();
+            ChartArea chartArea1 = new();
+            Legend legend1 = new();
             chart1 = new Chart();
             ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             SuspendLayout();
@@ -89,14 +89,11 @@ namespace Dino.Tools.WebMonitor
             chartArea1.Name = "ChartArea1";
             chart1.ChartAreas.Add(chartArea1);
             chart1.Dock = DockStyle.Fill;
-            legend1.Name = "Legend1";
-            chart1.Legends.Add(legend1);
             chart1.Location = new Point(0, 0);
             chart1.Margin = new Padding(4, 5, 4, 5);
             chart1.Name = "chart1";
             chart1.Size = new Size(379, 403);
             chart1.TabIndex = 0;
-            chart1.Text = "chart1";
             chart1.Click += chart1_Click;
             // 
             // Graph
@@ -106,9 +103,8 @@ namespace Dino.Tools.WebMonitor
             ClientSize = new Size(379, 403);
             Controls.Add(chart1);
             Margin = new Padding(4, 5, 4, 5);
-            Name = "Graph";
             Text = "Graph";
-            Load += Form1_Load;
+            Load += LoadData;
             ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
             ResumeLayout(false);
         }
